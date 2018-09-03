d3afd9d7aa10c8b95d84022ba9066bd1d78be140
Sergio Barbosa <sbarbosa115@gmail.com>
2018-09-03 08:43:05 -0500

    Integrated with laravel.mix

--- app/Http/Controllers/Frontend/TaxonomyController.php
+++ app/Http/Controllers/Frontend/TaxonomyController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Model\Product;
 use App\Model\Taxonomy;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaxonomyController extends Controller
 {
 
-    public function categories()
+    public function categories(): Response
     {
         $categories = Taxonomy::where("type", "=", "category")->with('products')->get();
         return response()->json($categories);
