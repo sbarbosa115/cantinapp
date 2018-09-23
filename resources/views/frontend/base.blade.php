0217ca8b6330afdb47ecc1b225d42762b15bdba4
Sergio Barbosa <sbarbosa115@gmail.com>
2018-09-23 08:14:59 -0500

    Changed title frontend app.

--- resources/views/frontend/base.blade.php
+++ resources/views/frontend/base.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="canonical" href="/" />
     <meta name="theme-color" content="#7796A8">
-    <title> Fast Food</title>
+    <title>Cantinapp - The new way to get your Cantina!</title>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playball:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Bitter:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
