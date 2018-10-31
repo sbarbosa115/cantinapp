ec9f01c6beda58bd72aa7c5f3bdfa18981ed5854
Sergio Barbosa <sbarbosa115@gmail.com>
2018-10-31 10:04:22 -0500

    Solved issues related with CSS and Header.

--- resources/assets/frontend/js/components/Search.js
+++ resources/assets/frontend/js/components/Search.js
@@ -1,107 +1,117 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Product from "./Product";
 import WelcomeHeader from "./WelcomeHeader";
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 class Search extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            items: [],
-            isLoaded : false,
-            query: false
-        };
+    this.state = {
+      items: [],
+      isLoaded: false,
+      query: false
+    };
 
-        this.onSearch = this.onSearch.bind(this);
-    }
+    this.onSearch = this.onSearch.bind(this);
+  }
 
-    componentDidMount() {
-        fetch(route('frontend.taxonomies.categories')).then(res => res.json()).then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    items: result
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
-    }
-
-    onSearch(e){
+  componentDidMount() {
+    fetch(route('frontend.taxonomies.categories')).then(res => res.json()).then(
+      (result) => {
         this.setState({
-           query: e.target.value
+          isLoaded: true,
+          items: result
         });
-    }
-
-    render() {
-        const _this = this;
-        const _products = [];
-
-        this.state.items.map((category, i) => {
-            category.products.map(item => {
-                if(item.status !== 'disabled'){
-                    _products.push(item)
-                }
-            });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
         });
+      }
+    )
+  }
 
-        const products = [];
-        _products.forEach((item, index) => {
-            if(_this.state.query && item.name.toLowerCase().search(_this.state.query.toLowerCase()) > -1){
-                products.push(<Product key={index} product={item}/>);
-            } else if(!_this.state.query){
-                products.push(<Product key={index} product={item}/>);
-            }
-        });
+  onSearch(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
 
-        return (
-            <div>
-                <WelcomeHeader/>
-                <section className="search-content">
-                    <div className="search-content-wrapper">
-                        <div className="container">
-                            <div className="row">
-                                <div className="search-content-group">
-                                    <div className="search-content-inner">
-                                        <div id="search">
-                                            <div className="expanded-message">
-                                                <div className="search-field">
-                                                    <form className="search" action="#" style={{position:'relative'}}>
-                                                        <input type="text" name="q" className="search_box" placeholder="search our store" autoComplete="off" onChange={e => this.onSearch(e)}/>
-                                                    </form>
-                                                </div>
-                                                {this.state.query && <div>
-                                                    <span className="subtext">Your search for '<strong>{this.state.query}</strong>' revealed the following: </span>
-                                                    <span className="results">{products.length} results found</span>
-                                                </div>}
-                                            </div>
-                                            <div className="product-item-group clearfix">
-                                                {products}
-                                            </div>
-                                            {products.length > 0 &&
-                                            <div className="search-bottom-toolbar">
-                                                <div className="search-pagination col-sm-6"></div>
-                                                <div className="search-counter col-sm-6">
-                                                    Items 1 to {products.length} of {_products.length} total
-                                                </div>
-                                            </div>
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+  render() {
+    const _this = this;
+    const _products = [];
+
+    this.state.items.map((category, i) => {
+      category.products.map(item => {
+        if (item.status !== 'disabled') {
+          _products.push(item)
+        }
+      });
+    });
+
+    const products = [];
+    _products.forEach((item, index) => {
+      if (_this.state.query && item.name.toLowerCase().search(_this.state.query.toLowerCase()) > -1) {
+        products.push(<Product key={index} product={item}/>);
+      } else if (!_this.state.query) {
+        products.push(<Product key={index} product={item}/>);
+      }
+    });
+
+    return (
+      <div>
+        <WelcomeHeader/>
+        <ScrollableAnchor id="start-order">
+          <section className="search-content">
+            <div className="search-content-wrapper">
+              <div className="container">
+                <div className="row">
+                  <div className="search-content-group">
+                    <div className="search-content-inner">
+                      <div id="search">
+                        <div className="expanded-message">
+                          <div className="search-field">
+                            <form className="search" action="#" style={{position: 'relative'}}>
+                              <input type="text"
+                                     name="q"
+                                     className="search_box"
+                                     placeholder="search our store"
+                                     autoComplete="off"
+                                     onChange={e => this.onSearch(e)}/>
+                            </form>
+                          </div>
+                          {this.state.query && <div>
+                            <span className="subtext">
+                              Your search for '<strong>{this.state.query}</strong>' revealed the following:
+                            </span>
+                            <span className="results">{products.length} results found</span>
+                          </div>}
                         </div>
+                        <div className="product-item-group clearfix">
+                          {products}
+                        </div>
+                        {products.length > 0 &&
+                        <div className="search-bottom-toolbar">
+                          <div className="search-pagination col-sm-6"></div>
+                          <div className="search-counter col-sm-6">
+                            Items 1 to {products.length} of {_products.length} total
+                          </div>
+                        </div>
+                        }
+                      </div>
                     </div>
-                </section>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </section>
+        </ScrollableAnchor>
+      </div>
+    );
+  }
 }
 
 export default Search;
--- resources/assets/frontend/js/components/WelcomeHeader.js
+++ resources/assets/frontend/js/components/WelcomeHeader.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Product from "./Product";
 
 class WelcomeHeader extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            imagePath : 'images/' + (Math.floor(Math.random() * 9) + 1) + '.jpg'
-        }
+    this.state = {
+      imagePath: 'images/' + (Math.floor(Math.random() * 9) + 1) + '.jpg'
     }
+  }
 
-    render() {
-        return (
-            <section className="home-welcome-layout zoomIn animated" data-animate="zoomIn" data-delay="200">
-                <div className="container">
-                    <div className="row">
-                        <div className="home-welcome-inner">
-                            <h2 className="page-title">Welcome to Cantinapp!</h2>
-                            <div className="home-welcome-content">
-                                    <span className="welcome-caption">
-                                        Desde ahora vas a poder ordenar tus comidas favoritas con anticipación y recogerlas en el momento que mas te convenga utilizando nuestra nueva e ingeniosa aplicación.
-                                        <br />
-                                        No filas, no esperas, solo has tu selección y te notificaremos cuando tu pedido este listo.
-                                        <br />
-                                        EMPEZAMOS?
-                                        <br />
-                                    </span>
-                                    <img className="welcome-banner" src={this.state.imagePath} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="home-welcome-layout zoomIn animated" data-animate="zoomIn" data-delay="200">
+        <div className="container">
+          <div className="row">
+            <div className="home-welcome-inner">
+              <h2 className="page-title">Welcome to Cantinapp!</h2>
+              <div className="home-welcome-content">
+                  <span className="welcome-caption">
+                      Desde ahora vas a poder ordenar tus comidas favoritas con anticipación y recogerlas
+                       en el momento que mas te convenga.
+                      <br/>
+                      No filas, no esperas, solo has tu selección y te notificaremos cuando tu pedido este listo.
+                      <br/>
+                       <a href='#start-order' className="btn btn-danger">
+                            ¿EMPEZAMOS?
+                       </a>
+                      <br/>
+                  </span>
+                <img className="welcome-banner" src={this.state.imagePath} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default WelcomeHeader;
